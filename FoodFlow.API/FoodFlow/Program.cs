fd9a26464a9e94afc51dd926b8adba290f9f8d50
Maheshshinde-7070 <maheshshinde7394@gmail.com>
2026-09-24 16:53:09 +0530

    Enable Swagger in production

--- FoodFlow.API/FoodFlow/Program.cs
+++ FoodFlow.API/FoodFlow/Program.cs
@@ -106,11 +106,10 @@ var app = builder.Build();
 app.UseCors("AllowAngular");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
